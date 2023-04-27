9eca2a3c470d1ae5e2b0ebb6a47da35cd843da52
MFindley08 <123705549+MFindley08@users.noreply.github.com>
2023-04-26 22:14:18 -0400

    added Moment.js and have live date and time. Added more "cities" to HTML

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,40 @@
+function updateTime() {
+  //Washington DC
+  let washingtonDcElement = document.querySelector("#washington-dc");
+  let washingtonDcDateElement = washingtonDcElement.querySelector(".date");
+  let washingtonDcTimeElement = washingtonDcElement.querySelector(".time");
+  let washingtonDcTime = moment().tz("America/New_York");
 
+  washingtonDcDateElement.innerHTML = moment().format("dddd, Do MMMM YYYY");
+  washingtonDcTimeElement.innerHTML = washingtonDcTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+  //Paris
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = parisTime.format("dddd, Do MMMM YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+
+  //London
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonTime = moment().tz("Europe/London");
+
+  londonDateElement.innerHTML = londonTime.format("dddd, Do MMMM YYYY");
+  londonTimeElement.innerHTML = londonTime.format("h:mm:ss[<small>]A[</small]");
+
+  //Tokyo
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+
+  tokyoDateElement.innerHTML = tokyoTime.format("dddd, Do MMMM YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss[<small>]A[</small]");
+}
+updateTime();
+setInterval(updateTime, 1);
