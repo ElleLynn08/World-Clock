6c889f4632a6203e6b2b3ff712c79b5e6462f61b
MFindley08 <123705549+MFindley08@users.noreply.github.com>
2023-04-29 13:32:17 -0400

    added HTML and JS for additional cities

--- javascript/index.js
+++ javascript/index.js
@@ -1,22 +1,25 @@
 function updateTime() {
+  //Seattle
+  let seattleElement = document.querySelector("#seattle");
+  let seattleDateElement = seattleElement.querySelector(".date");
+  let seattleTimeElement = seattleElement.querySelector(".time");
+  let seattleTime = moment().tz("US/Pacific");
+
+  seattleDateElement.innerHTML = seattleTime.format("dddd, Do MMMM YYYY");
+  seattleTimeElement.innerHTML = seattleTime.format(
+    "h:mm:ss[<small>]A[</small]"
+  );
+
   //Washington DC
   let washingtonDcElement = document.querySelector("#washington-dc");
   let washingtonDcDateElement = washingtonDcElement.querySelector(".date");
   let washingtonDcTimeElement = washingtonDcElement.querySelector(".time");
-  let washingtonDcTime = moment().tz("America/New_York");
+  let washingtonDcTime = moment().tz("US/Eastern");
 
   washingtonDcDateElement.innerHTML = moment().format("dddd, Do MMMM YYYY");
   washingtonDcTimeElement.innerHTML = washingtonDcTime.format(
     "h:mm:ss [<small>]A[</small>]"
   );
-  //Paris
-  let parisElement = document.querySelector("#paris");
-  let parisDateElement = parisElement.querySelector(".date");
-  let parisTimeElement = parisElement.querySelector(".time");
-  let parisTime = moment().tz("Europe/Paris");
-
-  parisDateElement.innerHTML = parisTime.format("dddd, Do MMMM YYYY");
-  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
 
   //London
   let londonElement = document.querySelector("#london");
@@ -27,6 +30,64 @@ function updateTime() {
   londonDateElement.innerHTML = londonTime.format("dddd, Do MMMM YYYY");
   londonTimeElement.innerHTML = londonTime.format("h:mm:ss[<small>]A[</small]");
 
+  //Paris
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = parisTime.format("dddd, Do MMMM YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+
+  //Moscow
+  let moscowElement = document.querySelector("#moscow");
+  let moscowDateElement = moscowElement.querySelector(".date");
+  let moscowTimeElement = moscowElement.querySelector(".time");
+  let moscowTime = moment().tz("Europe/Moscow");
+
+  moscowDateElement.innerHTML = moscowTime.format("dddd, Do MMMM YYYY");
+  moscowTimeElement.innerHTML = moscowTime.format("h:mm:ss[<small>]A[</small]");
+
+  //Baghdad
+  let baghdadElement = document.querySelector("#baghdad");
+  let baghdadDateElement = baghdadElement.querySelector(".date");
+  let baghdadTimeElement = baghdadElement.querySelector(".time");
+  let baghdadTime = moment().tz("Asia/Baghdad");
+
+  baghdadDateElement.innerHTML = baghdadTime.format("dddd, Do MMMM YYYY");
+  baghdadTimeElement.innerHTML = baghdadTime.format(
+    "h:mm:ss[<small>]A[</small]"
+  );
+
+  //Tehran
+  let tehranElement = document.querySelector("#tehran");
+  let tehranDateElement = tehranElement.querySelector(".date");
+  let tehranTimeElement = tehranElement.querySelector(".time");
+  let tehranTime = moment().tz("Asia/Tehran");
+
+  tehranDateElement.innerHTML = tehranTime.format("dddd, Do MMMM YYYY");
+  tehranTimeElement.innerHTML = tehranTime.format("h:mm:ss[<small>]A[</small]");
+
+  //Kabul
+  let kabulElement = document.querySelector("#kabul");
+  let kabulDateElement = kabulElement.querySelector(".date");
+  let kabulTimeElement = kabulElement.querySelector(".time");
+  let kabulTime = moment().tz("Asia/Kabul");
+
+  kabulDateElement.innerHTML = kabulTime.format("dddd, Do MMMM YYYY");
+  kabulTimeElement.innerHTML = kabulTime.format("h:mm:ss[<small>]A[</small]");
+
+  //Beijing
+  let beijingElement = document.querySelector("#beijing");
+  let beijingDateElement = beijingElement.querySelector(".date");
+  let beijingTimeElement = beijingElement.querySelector(".time");
+  let beijingTime = moment().tz("Asia/Hong_Kong");
+
+  beijingDateElement.innerHTML = beijingTime.format("dddd, Do MMMM YYYY");
+  beijingTimeElement.innerHTML = beijingTime.format(
+    "h:mm:ss[<small>]A[</small]"
+  );
+
   //Tokyo
   let tokyoElement = document.querySelector("#tokyo");
   let tokyoDateElement = tokyoElement.querySelector(".date");
@@ -36,5 +97,9 @@ function updateTime() {
   tokyoDateElement.innerHTML = tokyoTime.format("dddd, Do MMMM YYYY");
   tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss[<small>]A[</small]");
 }
+
 updateTime();
-setInterval(updateTime, 1);
+setInterval(updateTime, 1000);
+
+//let citySelectElement = document.querySelector("#city");
+//citySelectElement.addEventListener("change", updateCity);
